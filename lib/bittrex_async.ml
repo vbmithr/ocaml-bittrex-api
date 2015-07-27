604da99a16d72a19ef21eb253c6058057ce62637
Vincent Bernardoff <vb@luminar.eu.org>
2015-07-27 13:09:56 +0200

    Remove useless functions mk_uri.

--- lib/bittrex_async.ml
+++ lib/bittrex_async.ml
@@ -23,11 +23,10 @@ module Bitfinex = struct
     include AsyncIO
 
     let base_uri = "https://api.bitfinex.com/v1/"
-    let mk_uri section = Uri.of_string @@ base_uri ^ section
 
     let get endpoint params =
       let f () =
-        let uri = mk_uri endpoint in
+        let uri = Uri.of_string @@ base_uri ^ endpoint in
         Client.get Uri.(with_query' uri params) >>= fun (resp, body) ->
         Body.to_string body in
       trap_exn f
@@ -41,11 +40,10 @@ module BTCE = struct
 
     let version = "3"
     let base_uri = "https://btc-e.com/api/" ^ version ^ "/"
-    let mk_uri section = Uri.of_string @@ base_uri ^ section
 
     let get endpoint _ =
       let f () =
-        let uri = mk_uri endpoint in
+        let uri = Uri.of_string @@ base_uri ^ endpoint in
         Client.get uri >>= fun (resp, body) ->
         Body.to_string body in
       trap_exn f
@@ -59,11 +57,10 @@ module Bittrex = struct
 
     let version = "v1.1"
     let base_uri = "https://bittrex.com/api/" ^ version ^ "/"
-    let mk_uri section = Uri.of_string @@ base_uri ^ section
 
     let get endpoint params =
       let f () =
-        let uri = mk_uri endpoint in
+        let uri = Uri.of_string @@ base_uri ^ endpoint in
         Client.get Uri.(with_query' uri params) >>= fun (resp, body) ->
         Body.to_string body in
       trap_exn f
@@ -76,11 +73,10 @@ module Cryptsy = struct
 
     let version = "v2"
     let base_uri = "https://api.cryptsy.com/api/" ^ version ^ "/"
-    let mk_uri section = Uri.of_string @@ base_uri ^ section
 
     let get endpoint params =
       let f () =
-        let uri = mk_uri endpoint in
+        let uri = Uri.of_string @@ base_uri ^ endpoint in
         Client.get Uri.(with_query' uri params) >>= fun (resp, body) ->
         Body.to_string body in
       trap_exn f
@@ -107,11 +103,10 @@ module Kraken = struct
 
     let version = "0"
     let base_uri = "https://api.kraken.com/" ^ version ^ "/"
-    let mk_uri section = Uri.of_string @@ base_uri ^ section
 
     let get endpoint params =
       let f () =
-        let uri = mk_uri endpoint in
+        let uri = Uri.of_string @@ base_uri ^ endpoint in
         Client.get Uri.(with_query' uri params) >>= fun (resp, body) ->
         Body.to_string body in
       trap_exn f
@@ -125,11 +120,10 @@ module Hitbtc = struct
 
     let version = "1"
     let base_uri = "https://api.hitbtc.com/api/" ^ version ^ "/"
-    let mk_uri section = Uri.of_string @@ base_uri ^ section
 
     let get endpoint params =
       let f () =
-        let uri = mk_uri endpoint in
+        let uri = Uri.of_string @@ base_uri ^ endpoint in
         Client.get Uri.(with_query' uri params) >>= fun (resp, body) ->
         Body.to_string body in
       trap_exn f
